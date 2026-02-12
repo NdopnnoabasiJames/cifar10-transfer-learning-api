7484bef9e82c8f9d36c03703f5d0cf0e9eac3138
Ndopnno-abasi <ndopnnoabasi@gmail.com>
2026-02-12 19:24:30 +0100

    Added logging

--- app/main.py
+++ app/main.py
@@ -3,20 +3,29 @@ from model_loader import model, CLASS_NAMES
 from utils import preprocess_image
 from PIL import Image
 import numpy as np
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI(title="CIFAR10 Transfer Learning API")
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    
-    image = Image.open(file.file).convert("RGB")
-    processed = preprocess_image(image)
+    try:
+        logger.info(f"Prediction request received: {file.filename}")
 
-    predictions = model.predict(processed)
-    class_index = int(np.argmax(predictions))
-    confidence = float(np.max(predictions))
+        image = Image.open(file.file).convert("RGB")
+        processed_image = preprocess_image(image)
 
-    return {
-        "prediction": CLASS_NAMES[class_index],
-        "confidence": round(confidence, 4)
-    }
+        predictions = model.predict(processed_image)
+        class_index = int(np.argmax(predictions))
+        confidence = float(np.max(predictions))
+
+        return {
+            "prediction": CLASS_NAMES[class_index],
+            "confidence": round(confidence, 4)
+        }
+    except Exception as e:
+        logger.error(f"Error during prediction: {e}")
+        return {"error": "An error occurred during prediction."}
